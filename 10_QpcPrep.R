212cabc62301d5c6f238c7fe1ca9ca9a362a59c7
sfbuysse <sfbuysse@gmail.com>
2023-11-16 10:19:08 -0500

    Qpc update

--- 10_QpcPrep.R
+++ 10_QpcPrep.R
@@ -1,6 +1,7 @@
 # Sophie Buysse
 # 8/3/2023
-# making file with 50,000 randomly selected SNPs for Emily to do Qpc with
+# updated 11/16/2023
+# making file with 50,000 randomly selected SNPs for Qpc
 
 # HPCC set up if looking at this and not running it as a job
 #module purge
@@ -8,50 +9,55 @@
 # if submitting, need:
 #SBATCH --constraint="[intel16|intel18|amd20|amd22]"
 
+##### load libraries ######
+library(genio)
+
 ##### Genotype Info #####
 
 # set seed
 set.seed(803)
 
 # load files
-load(file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_plinkFiltered_Cent_Dec2022.ROBJ")
-cent_matrix <- tmp2
-rm(tmp2)
-load(file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_plinkFiltered_NoCent_Dec2022.ROBJ")
-nocent_matrix <- tmp2
-rm(tmp2)
+# want the file where each input is a 0, 1, 2 - NOT where it is already in letter format.
+# this is reading in the allsites plink filtered file that is the same input file for Fst, PCA, and GWAS.
+SNPs_Cent <- read_plink("/mnt/scratch/buysseso/GVCF/allsites_filtered_plinkTest", verbose = TRUE)
+# I think X is what I want
+head(SNPs_Cent$X)[1:5, 1:5]
+# but I want it transposed
+Stamen_Gt <- t(SNPs_Cent$X)
+head(Stamen_Gt)[1:5, 1:5]
 
-## subset 50,000 randomly chosen SNPs for fst matrix ##
-
+## subset 50,000 randomly chosen SNPs ##
 # this does include the centromeres.
-length(cent_matrix$SNP_ID)
-rows_keep <- runif(n = 50000, min = 2, max = length(cent_matrix$SNP_ID))
-rows_keep <- c(1,rows_keep)
-# need to keep row one for the ID, set rows_keep to start at two so I never have row 1 twice
-# want to keep all columns because each column is an individual, only filter rows
-cent_sub <- cent_matrix[rows_keep, ]
-str(cent_sub)
-class(cent_sub)
-length(cent_sub$SNP_ID)
-#50,001
 
-save(cent_sub, file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_Cent_50k_Aug2023.ROBJ")
-write.csv(cent_sub, file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_Cent_50k_Aug2023.csv", row.names = FALSE)
+# SNPs are cols, so find n.cols
+ncol(Stamen_Gt)
+cols_keep <- runif(n = 50000, min = 1, max = ncol(Stamen_Gt))
+# double check no repeat numbers
+length(unique(cols_keep))
+# the individual ID is the rowname, and I need to make sure that stays.
+Stamen_Gt_sub <- Stamen_Gt[ , cols_keep]
+head(Stamen_Gt_sub)[1:5, 1:5]
 
-# this does not include the centromeres.
-length(nocent_matrix$SNP_ID)
-rows_keep2 <- runif(n = 50000, min = 2, max = length(nocent_matrix$SNP_ID))
-rows_keep2 <- c(1,rows_keep2)
-# need to keep row one for the ID, set rows_keep to start at two so I never have row 1 twice
-# want to keep all columns because each column is an individual, only filter rows
-nocent_sub <- nocent_matrix[rows_keep, ]
-str(nocent_sub)
-class(nocent_sub)
-length(nocent_sub$SNP_ID)
-#50,001
+str(Stamen_Gt_sub)
+class(Stamen_Gt_sub)
+tmp <- Stamen_Gt_sub[!complete.cases(Stamen_Gt_sub), ]
+# hmm. these NAs will be a problem later on b/c make_k would make a matrix of all NA (even after dividing by 2).
+# so do I need to.... do what?
+# quaint documentation says to use random imputation to replace missing genotypes. how??
 
-save(nocent_sub, file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_NoCent_50k_Aug2023.ROBJ")
-write.csv(nocent_sub, file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_NoCent_50k_Aug2023.csv", row.names = FALSE)
+# what if I subsample just the complete cases from the initial matrix? are there any?
+tmp2 <- Stamen_Gt[complete.cases(Stamen_Gt), ]
+# HA it is empty.
+# I think I need another program to do imputation, and that would happen before I even start the subsetting in R.
+# ok. well it looks like I"ll need to use BEAGLE to do this. and that requires data in a vcf format. and downloading another program and stuff
+# I don't have it in me to do that now.
+
+
+
+#save(cent_sub, file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_Cent_50k_Aug2023.ROBJ")
+#write.csv(cent_sub, file = "/mnt/research/josephslab/Sophie/Qpc/GenotypeMatrix_Cent_50k_Aug2023.csv", row.names = FALSE)
+
 
 ##### Phenotype Information #####
 # read in FAM... how have I done this before?
