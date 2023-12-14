ef5a6c5765761e2bcf7109173ec2c28247507146
sfbuysse <sfbuysse@gmail.com>
2023-12-14 11:44:45 -0500

    Clean Map Code

--- README.md
+++ README.md
@@ -14,8 +14,8 @@ Most of the data is in my scratch space, but the most important things should be
 - 02_CallVariants: not currently any figures. The end result is the raw allsites file of genotypes.
 - 03_FilterVariants:not currently any figures. is the code to go from raw to filtered variant and invariant sites before doing pixy (vcf) and to make the filtered variant sites file used for Fst, GWAS, and PCA (plink format). It also removes the centromere regions post filtering.
 - 04_PopulationRawMeans_LSMs: no figures. calculates LSMs and compares them to the raw means (so the correlation values in the methods section come from here). This makes the Elev_means_Feb2022 ROBJ that is used in scripts 6 and 11.  there is another file with an identical name besides the 04 - I likely made this file with another R script and then renamed it.
-- 05_Pixy_pi_fst: no figures, just the bash code for running pixy
-- 06_Pixy_pi_04082022: many figures! pi by elevation (cent and no cent - fig 2), ssn by elevation, SSN by pi (cent and no cent - fig 3), both residual multiple regression figures (no cent only - fig 3) 
+- 05_Pixy_pi: no figures, just the bash code for running pixy (all code also calculated Fst with pixy)
+- 06_Pixy_pi_04082022: many figures! pi by elevation (cent and no cent - fig 2), ssn by elevation, SSN by pi (cent and no cent - fig 3), both residual multiple regression figures (no cent only - fig 3) - this one has an old code file as well that I no longer use.
 - 07_Fst_diveRsity_2022: makes the Fst heatmap for cent and no cent. only in supplemental, not main manuscript
 - 08_PCA_2022: runs PCA and makes figures for cent and no cent for first 4 PCs (fig 2). Also does regressions of PCs with elevation.
 - 09_GWAS: makes all the GWAS manhattan and qqplots for each of the 4 types of GWAS with and without the centromere. Also does the shared SNP analysis.
--- elevation_map_all my code.R
+++ elevation_map_all my code.R
@@ -11,6 +11,8 @@
 # https://geocompr.robinlovelace.net/adv-map.html
 # not used, but for reference, got shapefile from here: 
 # https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2/gis-files/spain-shapefile
+# to load:
+# spain <- read_sf("C:/Users/Sophia/Downloads/Spain_shapefile/es_1km.shp")
 
 ##### Code #####
 ## load libraries:
@@ -27,11 +29,11 @@ data("World")
 spain <- World[World$iso_a3 == "ESP", ]
 
 # load the population metadata for population locations
-metadata <- read.csv("SpanishMasterDataset_PopMetaData.csv")
+metadata <- read.csv("C:/Users/Sophie/Michigan State University/Conner, Jeffrey - SophieAnalyses/SpanishMasterDataset_PopMetaData.csv")
 metadata <- metadata[1:16, ]
 
 # load the elevation raster, downloaded from WorldClim. Highest resolution is best because on small scale.
-elev_raster = raster("WorldClim/Elev/wc2.1_30s_elev.tif")
+elev_raster = raster("C:/Users/Sophie/Michigan State University/Conner, Jeffrey - SophieAnalyses/WorldClim/Elev/wc2.1_30s_elev.tif")
 
 # crop elevation raster down to the size of spain
 spain_elev <- crop(elev_raster, spain)
@@ -40,7 +42,7 @@ plot(spain_elev)
 # Make a spatial polygon that includes only the pyrenees region
 # got extent values from the min and max lat and long of the populations (originally -0.50, 3.5, 41.00, 43.00)
 # but max x is actually 3.16, so changes to 3.2
-# but this means the pyrenees extend out beyond the limits of spain... because the spain elev raster x max is 3.04
+# but this means the pyrenees extend out beyond the limits of spain... becuase the spain elev raster x max is 3.04
 pyrenees <- as(extent(-0.50, 3.2, 41.00, 43.00), 'SpatialPolygons')
 
 # set the coordinate reference system to match the elevation and World dataset
@@ -48,6 +50,9 @@ crs(pyrenees) <- "+proj=longlat +datum=WGS84 +no_defs"
 
 # Get the location for points and set coordinate reference system
 pop_loc <- data.frame(lon = metadata$Lon_DecDeg, lat = metadata$Lat_DecDeg, pop = metadata$PopCode) %>%
+  st_as_sf(coords = c("lon", "lat"), crs = '+proj=longlat +datum=WGS84 +no_defs ')
+
+pop_coc <- data.frame(lon = c(metadata$Lon_DecDeg, 3.19), lat = c(metadata$Lat_DecDeg,42.31), pop = c(metadata$PopCode, "COC_Cast")) %>%
   st_as_sf(coords = c("lon", "lat"), crs = '+proj=longlat +datum=WGS84 +no_defs ')
 
 st_is_longlat(pop_loc)
@@ -61,7 +66,7 @@ outline <- tm_shape(spain) +
   tm_shape(pyrenees) +
   tm_borders("red", lwd = 1)
 outline
-tmap_save(outline, "Figures/OutlineMap.png", dpi = 1000)
+tmap_save(outline, "C:/Users/Sophie/Michigan State University/Conner, Jeffrey - SophieAnalyses/Figures/OutlineMap.png", dpi = 1000)
 
 # Then, make the detailed map
 # Now crop elevation raster down to pyrenees region, with a little buffer for labels to fit
@@ -80,6 +85,61 @@ st_is_longlat(pop_loc2)
 
 tmap_mode("plot")
 
+# add columns for color and shape control
+# manually checked with scheme from other figures. could have done this with more automation with rep() and ordering by elevation?
+pop_loc$for.cols <- as.factor(c("red", "red", "red", "green", "yellow", "red", "green",
+                                "blue", "yellow", "yellow", "blue", "green", "green", "blue", "blue", "yellow"))
+pop_loc$for.shape <- as.factor(c(2,0,1,0,0,5,1,0,2,1,5,2,5,2,1,5))
+# as.factor is important.
+pop_loc$pop <- as.factor(pop_loc$pop)
+
+tm_shape(pyr_elev)+
+  tm_raster(palette = terrain.colors(10), alpha = 0.6, title = "Elevation")+
+  tm_scale_bar(position = c("right", "bottom"))+
+  tm_legend(legend.position = c("right", "bottom"))+
+  tm_shape(pop_loc2)+
+  tm_text('pop', col = 'black', auto.placement = FALSE)+
+  tm_shape(pop_loc)+
+  tm_symbols('for.cols', palette=c(blue = 'blue', green = 'green', red = 'red', yellow = 'orange'), 
+             stretch.palette = FALSE, size = 0.5, shape = 'for.shape', 
+             shapes = c('0' = 22, '1' = 21, '2' = 24, '5' = 23), 
+             legend.col.show = FALSE, legend.shape.show =  FALSE)
+
+
+
+## trying to find elevation of this population. based on this, it should be the 500 number and not the 200 number.
+tmp_coc <- crop(elev_raster, as(extent(3.0, 3.5, 42.2, 42.4), 'SpatialPolygons'))
+tm_shape(tmp_coc)+
+  tm_raster(palette = terrain.colors(10), alpha = 0.6, title = "Elevation")+
+  tm_scale_bar(position = c("right", "bottom"))+
+  tm_legend(legend.position = c("right", "bottom"))+
+  tm_shape(pop_loc2)+
+  tm_text('pop', col = 'black', auto.placement = FALSE)+
+  tm_shape(pop_coc)+
+  tm_symbols()
+
+#8/3/2023 update - working on different coloring scheme
+# add columns for color and shape control
+# manually checked with scheme from other figures. could have done this with more automation with rep() and ordering by elevation?
+pop_loc$for.cols <- as.factor(c("red", "red", "red", "green", "yellow", "red", "green",
+                                     "blue", "yellow", "yellow", "blue", "green", "green", "blue", "blue", "yellow"))
+pop_loc$elev <- as.factor(c(1229, 416, 441, 1444, 715, 519, 413, 836, 1585, 1706, 177, 665, 61, 332, 991, 1605))
+pop_loc$for.shape <- as.factor(c(2,0,1,0,0,5,1,0,2,1,5,2,5,2,1,5))
+# as.factor is important.
+pop_loc$pop <- as.factor(pop_loc$pop)
+# try to color like background
+tm_shape(pyr_elev)+
+  tm_raster(palette = terrain.colors(16), alpha = 0.6, title = "Elevation")+
+  tm_scale_bar(position = c("right", "bottom"))+
+  tm_legend(legend.position = c("right", "bottom"))+
+  tm_shape(pop_loc2)+
+  tm_text('pop', col = 'black', auto.placement = FALSE)+
+  tm_shape(pop_loc)+
+  tm_symbols('elev', palette=terrain.colors(16), 
+    stretch.palette = TRUE, size = 0.5, shape = 'for.shape', 
+    shapes = c('0' = 22, '1' = 21, '2' = 24, '5' = 23), 
+    legend.col.show = TRUE, legend.shape.show =  FALSE)
+# just make a black point 
 black <-  tm_shape(pyr_elev)+
     tm_raster(palette = terrain.colors(16), alpha = 0.8, title = "Elevation")+
     tm_scale_bar(position = c("right", "bottom"))+
@@ -91,7 +151,7 @@ black <-  tm_shape(pyr_elev)+
                stretch.palette = TRUE, size = 0.25, 
                legend.col.show = FALSE, legend.shape.show =  FALSE)
 black
-tmap_save(black, "Figures/ManuscriptFigs/BlackDotMap.png", 
+tmap_save(black, "C:/Users/Sophie/Michigan State University/Conner, Jeffrey - SophieAnalyses/Figures/ManuscriptFigs/BlackDotMap.png", 
           dpi = 1000,
           outer.margins = c(0, 0, 0, 0),
           insets_tm = outline, 
