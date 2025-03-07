0be2e8bca226856138a09b4d8704219653058433
sfbuysse <sfbuysse@gmail.com>
2025-03-07 13:01:36 -0500

    add elev labels to map

--- MessyCode/elevation_map_all my code.R
+++ MessyCode/elevation_map_all my code.R
@@ -191,3 +191,49 @@ black2
 # I don't like how this looks
 
 # put legend and inset on same side?
+
+# version with elevation added to pop label
+pop_loc3 <- data.frame(lon = c(metadata$Lon_DecDeg[1:6], metadata$Lon_DecDeg[7]-0.04, 
+                               metadata$Lon_DecDeg[8:10], metadata$Lon_DecDeg[11]+0.04,
+                               metadata$Lon_DecDeg[12:16]), 
+                       lat = c(metadata$Lat_DecDeg[1:5]+0.06, metadata$Lat_DecDeg[6]-0.06, metadata$Lat_DecDeg[7]-0.06, 
+                               metadata$Lat_DecDeg[8]+0.06, metadata$Lat_DecDeg[9]-0.06,
+                               metadata$Lat_DecDeg[10:16]+0.06), 
+                       pop = paste0(metadata$PopCode, " - ", metadata$Elev_m,"m")) %>%
+  st_as_sf(coords = c("lon", "lat"), crs = '+proj=longlat +datum=WGS84 +no_defs ')
+st_is_longlat(pop_loc3)
+
+black3 <-  tm_shape(pyr_elev)+
+  tm_raster(palette = terrain.colors(16), alpha = 0.8, title = "Elevation")+
+  tm_scale_bar(position = c("center", 0), text.size = 1.1)+ # x and y coordinates I was trying c(0.04, 0.005)
+  tm_legend(scale = 0.4, legend.outside = FALSE, legend.position = c("left", "bottom"), legend.text.size = 1.1, legend.title.size = 1.5,
+            legend.bg.color = "white", legend.frame = "black")+
+  tm_shape(pop_loc3)+
+  tm_text('pop', col = 'black', auto.placement = FALSE, size = 1.1)+
+  tm_shape(pop_loc)+
+  tm_symbols(col = "black", border.col = "black", shape = 21, 
+             stretch.palette = TRUE, size = 0.5, 
+             legend.col.show = FALSE, legend.shape.show =  FALSE)
+black3
+
+tmap_save(black3, "C:/Users/Sophie/Michigan State University/Conner, Jeffrey - SophieAnalyses/Figures/ManuscriptFigs/BlackDotMap_ElevLabel.png", 
+          dpi = 1000,
+          outer.margins = c(0, 0, 0, 0),
+          insets_tm = outline, 
+          insets_vp = viewport(x = unit(0.88, "npc"),
+                               y = unit(0.135, "npc"),
+                               width = unit(0.25, "npc"),
+                               height = unit(0.25, "npc"),
+                               default.units = "npc",
+                               just = "centre",gp = gpar(),
+                               clip = "inherit",
+                               mask = "inherit",
+                               xscale = c(0, 1),
+                               yscale = c(0, 1),
+                               angle = 0,
+                               layout = NULL,
+                               layout.pos.row = NULL, layout.pos.col = NULL,name = NULL),
+          width = 3,
+          height = 2,
+          units = 'in',
+          add.titles = "test")
